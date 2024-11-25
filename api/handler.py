22a5417fd334ac410672a793cad0a0ff6e8ade1e
ArthurHottgen <arthurhottgen@gmail.com>
2024-11-25 11:54:25 -0300

    added port to api gateway

--- api/handler.py
+++ api/handler.py
@@ -40,4 +40,4 @@ def health_insurance_predict():
         return Response( '{"response": "No data provided"}', status=200, mimetype='application/json' )
     
 if __name__ == '__main__':
-    app.run( '0.0.0.0', debug = True )
+    app.run( '0.0.0.0', port=5000, debug = True )
